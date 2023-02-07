87cae486b319d19fd531ec3120d66bc02ddbc2d6
ssssojung <1216thwjd@naver.com>
2023-02-07 22:00:36 +0900

    99강 - 100강

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "../UI/Card";
+// import Button from "../UI/Button"
+import classes from "./AddUser.module.css";
+
+function AddUser(props) {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+    );
+}
+
+export default AddUser;
