2f5c04633589bc2b22640014b849f811eb26b90e
ssssojung <1216thwjd@naver.com>
2023-02-08 10:38:58 +0900

    101강

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
-// import Button from "../UI/Button"
+import Button from "../UI/Button"
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
@@ -15,7 +15,7 @@ function AddUser(props) {
                 <input id="username" type="text" />
                 <label htmlFor="age">Age (Years)</label>
                 <input id="age" type="number" />
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
