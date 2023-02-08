3896ad0a09eaa4015a42abbc162eaa5dab2eba48
ssssojung <1216thwjd@naver.com>
2023-02-08 17:22:06 +0900

    107강

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,13 +7,21 @@ import classes from "./AddUser.module.css";
 function AddUser(props) {
     const [enteredUserName, setEnteredUserName] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-
+    const [error, setError] = useState();
     const addUserHandler = (event) => {
         event.preventDefault();
         if(enteredUserName.trim().length===0 || enteredAge.trim().length===0){
+            setError({
+               title: 'Invalid input',
+               message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         };
         if(+enteredAge < 1){
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age(> 0).'
+             });
             return;
         }
         props.onAddUser(enteredUserName, enteredAge);
@@ -27,10 +35,13 @@ function AddUser(props) {
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value)
     };
+    const errorHandler = (event) => {
+        setError(null);
+    };
 
     return (
         <div>
-            <ErrorModal title='An error occured!' message='Something went wrong!'/>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
